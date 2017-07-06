b7832aade1fffaea6c53e2ef71046a2295035701
mrockstyle <m.guitarkung2@gmail.com>
2017-07-06 15:12:53 +0700

    fix bot code

--- bot.php
+++ bot.php
@@ -22,11 +22,12 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => 'สวัสดีครับ'
 			];
-			}
-			$messages = [
+			}else{
+				$messages = [
 				'type' => 'text',
 				'text' => 'hello'
 			];
+			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
